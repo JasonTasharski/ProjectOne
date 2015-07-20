dcd66fea6af02c1b381466444b957cbb680a555c
J T <jtasharski@gmail.com>
2015-07-20 14:09:31 -0700

    Favicon

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 $(function() {
 
 	console.log("app.js loaded");
-	
-	request("https://www.kimonolabs.com/api/7imxs5lg?kimbypage=1?apikey=P2npdGDmBAziqQB7UEgQR01k1joldB86", 
-		function(err, response, body) {
-		  console.log(body);
+
+	getEvents: function() {
+		request("https://www.kimonolabs.com/api/7imxs5lg?kimbypage=1?apikey=P2npdGDmBAziqQB7UEgQR01k1joldB86", 
+			function(err, response, body) {
+			  console.log(body);
+		});
+	}
+
+
+	$.get('/api/events', function() {
+		getEvents();
 	});
+
 
 });

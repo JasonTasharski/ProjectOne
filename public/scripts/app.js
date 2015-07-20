8659613b56a8a2b92a8abddc708e1923a7ba5e7c
J T <jtasharski@gmail.com>
2015-07-20 14:59:53 -0700

    More debugging

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,16 +2,11 @@ $(function() {
 
 	console.log("app.js loaded");
 
-	getEvents: function() {
+	$.get('/api/events', function() {
 		request("https://www.kimonolabs.com/api/7imxs5lg?kimbypage=1?apikey=P2npdGDmBAziqQB7UEgQR01k1joldB86", 
 			function(err, response, body) {
 			  console.log(body);
 		});
-	}
-
-
-	$.get('/api/events', function() {
-		getEvents();
 	});
 
 

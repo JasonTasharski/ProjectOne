98c98c7436af157ab1b9cc3091ad462f457f80f9
J T <jtasharski@gmail.com>
2015-07-21 14:24:49 -0700

    Testing with Chhun

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,55 +5,53 @@ $(function() {
 	var templateR = _.template($('#reportTemplate').html()); // compile
 	var templateE = _.template($('#eventTemplate').html()); // compile
 
-	var renderR = function(reportToRender) {
-	  $('#reportTarget').append($(templateR(reportToRender))); // will render reports
+	var renderR = function(reportToRender, eventToRender) { // will render reports
+		console.log("Rendering report...");
+		reportTargetP = eventToRender.reportTargetP;
+	  console.log("Targeting " + "'" + reportTargetP + "'");
+	  // $(eventTemplate).append($(templateR(reportTemplate)));
+	  // $(reportTargetP).append($(templateR(reportToRender)));
+	  $('#testingWithChhun').append($(templateR(reportToRender)));
+	  console.log("Report rendered.");
 	};
 	var renderE = function(eventToRender) {
+		console.log("Rendering EVENT...");
 	  $('#eventTarget').append($(templateE(eventToRender))); // will render events
 		_.each(eventToRender.reports, function(reportToRender) {
-			renderR(reportToRender); // called, calls renderR on each assigned report
+			renderR(reportToRender, eventToRender); // called, calls renderR on each assigned report
 		});
+		console.log("EVENT rendered.");
 	};
 
 	// Hard-coding for fun and profit
-	var eventHC1 = {
-		eventLocationP: "Out of place",
-		eventDateP: "Out of time",
-		reports: [
-			{
-				iAmA: "Hard-coded report #1",
-				reportTitleP: "First Report",
-				reportLocationP: "Jharkhand",
-				reportDateP: "Yesterday",
-			},
-			{
-				iAmA: "Hard-coded report #2",
-				reportTitleP: "Second Report",
-				reportLocationP: "Chhatisgarh",
-				reportDateP: "Tomorrow",
-			}
-		]
+	var eventArr = [];
+	var ReportP = function(reportTitleP, reportLocationP, reportDateP){
+		this.reportTitleP = reportTitleP;
+		this.reportLocationP = reportLocationP;
+		this.reportDateP = reportDateP;
+		console.log("Report created: "+ this.reportTitleP);
 	}
-	var eventHC2 = {
-		eventLocationP: "Out of place",
-		eventDateP: "Out of time",
-		reports: [
-			{
-				reportTitleP: "Third Report",
-				reportLocationP: "Kashmir",
-				reportDateP: "Last Week",
-			},
-			{
-				reportTitleP: "Fourth Report",
-				reportLocationP: "Nagaland",
-				reportDateP: "Next Week",
-			}
-		]
+	var EventP = function(eventLocationP, eventDateP){
+		this.eventLocationP = eventLocationP;
+		this.eventDateP = eventDateP;
+		this.reports = [];
+		this.reportTargetP = "#" + eventArr.length;
+		eventArr.push(this);
+		console.log("EVENT created.");
 	}
-
+	console.log("Creating events and reports...");
+	var r1 = new ReportP("First Report", "Jharkhand", "Yesterday");
+	var r2 = new ReportP("Second Report", "Chhatisgarh", "Tomorrow");
+	var r3 = new ReportP("Third Report", "Kashmir", "Last Week");
+	var r4 = new ReportP("Fourth Report", "Nagaland", "Next Week");
+	var eventHC1 = new EventP("Out of place", "Out of time");
+	var eventHC2 = new EventP("Somewhere", "Sometime");
+	eventHC1.reports.push(r1, r2);
+	eventHC2.reports.push(r3, r4);
+	console.log("EVENTs filled.");
 	renderE(eventHC1);
 	renderE(eventHC2);
-
+	console.log("EVENTs rendered.");
 	// $.get('/api/events', function() {
 	// 	request("https://www.kimonolabs.com/api/7imxs5lg?kimbypage=1?apikey=P2npdGDmBAziqQB7UEgQR01k1joldB86", 
 	// 		function(err, response, body) {
